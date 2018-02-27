60ac33efeac7d77337adfa1f2cdc2afa19fa14cf
Alex Kobilianskiy <alex_kobilianskiy@bigmir.net>
2018-02-27 19:23:11 +0200

    header complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
-	// Custom JS
+    $(".toggle-mnu").click(function() {
+        $(this).toggleClass("on");
+        $(".mobile-mnu").slideToggle();
+        $(".welcome-section").toggleClass("welcome-section-mover");
+        return false;
+    });
+
+
 
 });
